416fdd0a8b4e3fd98c6eea3b53dd9ffbd4077c2e
fightlee01 <dan_dan_lee@163.com>
2021-01-01 17:27:16 +0800

    test

---
+++ 01_day/DataType.java
@@ -0,0 +1,13 @@
+public class DataType {
+    public static void main(String args[]) {
+        // int转为long
+        long num1 = 100;
+        System.out.println(num1);
+        // float转为double
+        double num2 = 3.1F;
+        System.out.println(num2);
+        // long转为float
+        float num3 = 10000L;
+        System.out.println(num3);
+    }
+}
